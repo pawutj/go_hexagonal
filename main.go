ce21f9d7651a05843201a903592197bb1a5a0959
astralaria <pawut.j@gmail.com>
2023-01-09 02:07:23 +0700

    Add viper

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/pawutj/go_hexagonal/repository"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	url := ""
+	url := fmt.Sprintf("%v:%v@tcp:%v", viper.GetString("db.username"), viper.GetString("db.password"))
 	db, err := sqlx.Open("mysql", url)
 	if err != nil {
 		panic(err)
@@ -17,4 +20,15 @@ func main() {
 
 	customers, err := customerRepository.GetAll()
 
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }
